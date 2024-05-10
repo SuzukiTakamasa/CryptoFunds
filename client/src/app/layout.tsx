d35d83d0e7068a8f26fadeaa3027e95d2f37595c
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-05-11 01:06:12 +0900

    add Link to layout.tsx

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,10 @@
+"use client"
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+
+import Link from "next/link"
 
 const inter = Inter({ subsets: ["latin"] });
 
