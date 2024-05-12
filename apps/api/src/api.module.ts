c77db8690decf5d68d70319a0a925efd5f0a47ba
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-05-12 22:38:18 +0900

    add @nest/graphql and @nest/apollo

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,9 +1,17 @@
-import { Module } from '@nestjs/common';
-import { ApiController } from './api.controller';
-import { ApiService } from './api.service';
+import { Module } from '@nestjs/common'
+import { ApiController } from './api.controller'
+import { ApiService } from './api.service'
+
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    })
+  ],
   controllers: [ApiController],
   providers: [ApiService],
 })
