dcaf2b3a3d344790a7e61fed6df219fe9c177aaa
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-06-28 01:33:04 +0900

    add google.strategy.ts

--- cryptofunds-backend/src/app.module.ts
+++ cryptofunds-backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { GoogleStrategy } from './auth/google.strategy'
 
 @Module({
   imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, GoogleStrategy],
 })
 export class AppModule {}
