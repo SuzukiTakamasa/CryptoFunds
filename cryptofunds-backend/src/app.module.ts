773b1633db3d6c50ce5735fadccb1a274417ea5d
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-06-28 23:47:02 +0900

    add typeorm to app.module.ts as postgresql client

--- cryptofunds-backend/src/app.module.ts
+++ cryptofunds-backend/src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { GoogleStrategy } from './auth/google.strategy'
+import { join } from 'path'
+
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user',
+      password: 'password',
+      database: 'test',
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      synchronize: true
+    })],
   controllers: [AppController],
   providers: [AppService, GoogleStrategy],
 })
