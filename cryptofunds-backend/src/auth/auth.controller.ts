4929f47ddbd2b21c62e89a848b4b302a338a797d
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-06-28 23:58:23 +0900

    add googleCallback get endpoint with some comments

--- cryptofunds-backend/src/auth/auth.controller.ts
+++ cryptofunds-backend/src/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Request, Response } from 'express'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+   @Get('google/callback')
+   async googleCallback(@Req() req: Request, @Res() res: Response) {
+    const code = req.query.code as string
+    //const jwtToken = await this.authService.getGoogleToken(code)
+    //res.cookie('access_token', jwtToken, { httpOnly: true})
+    return res.redirect('dashboard')
+   }
 }
